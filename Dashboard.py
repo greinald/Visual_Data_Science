c9379f93f071e38cd08aa4edfe5948ce0abcafc2
greinald <123128464+greinald@users.noreply.github.com>
2024-12-28 14:38:23 +0100

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -4,7 +4,7 @@ import plotly.express as px
 from sklearn.preprocessing import MinMaxScaler
 
 # Load the dataset
-df = pd.read_csv('/Users/greinaldpappa/Desktop/TU/Visualisation_DS/Code/Final_Data.csv')
+df = pd.read_csv('https://github.com/greinald/Visual_Data_Science/blob/main/Final_Data.csv')
 
 # Streamlit app layout
 st.title("Interactive Dashboard for European Data")
